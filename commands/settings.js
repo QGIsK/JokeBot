22935b76a677b58f4d83072e05259cf33a419242
Demian <Devaccdemiann@gmail.com>
2021-10-10 19:18:26 +0200

    Cleanup

--- commands/settings.js
+++ commands/settings.js
@@ -6,21 +6,14 @@ module.exports = {
   data: new SlashCommandBuilder().setName('settings').setDescription('See current settings'),
 
   async execute(interaction, settings) {
-    console.log(
+    const settingsEmbed = new MessageEmbed().setTitle('Settings for JokeBot').addFields(
       { name: 'safemode', value: `${settings.safemode ? 'on' : 'off'}`, inline: true },
       { name: 'lang', value: settings.lang, inline: true },
-      { name: 'blacklist', value: settings.blacklist.join(', ') ?? 'No blacklisted flags yet.' }
+      {
+        name: 'blacklist',
+        value: settings.blacklist.length > 0 ? settings.blacklist.join(', ') : 'No blacklisted flags yet.',
+      }
     );
-    const settingsEmbed = new MessageEmbed()
-      .setTitle('Settings for JokeBot')
-      .addFields(
-        { name: 'safemode', value: `${settings.safemode ? 'on' : 'off'}`, inline: true },
-        { name: 'lang', value: settings.lang, inline: true },
-        {
-          name: 'blacklist',
-          value: settings.blacklist.length > 0 ? settings.blacklist.join(', ') : 'No blacklisted flags yet.',
-        }
-      );
 
     await interaction.reply({ embeds: [settingsEmbed] });
   },
