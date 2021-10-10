688371bd61d44360efab6405ff0a95a6e7d5067a
Demian <Devaccdemiann@gmail.com>
2021-10-10 19:28:32 +0200

    Add category option

--- commands/joke.js
+++ commands/joke.js
@@ -3,9 +3,14 @@ const JokeWrapper = require('@qgisk/jokeapi-wrapper');
 const JokeClient = new JokeWrapper();
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('joke').setDescription('Replies with a joke!'),
+  data: new SlashCommandBuilder()
+    .setName('joke')
+    .setDescription('Replies with a joke!')
+    .addStringOption((option) => option.setName('category').setDescription(`${JokeWrapper.CATEGORIES.join(', ')}.`)),
   async execute(interaction, settings) {
-    const output = await JokeClient.getJoke(settings);
+    const category = interaction.options.get('category');
+
+    const output = await JokeClient.getJoke({ ...settings, categories: category ? category.value : 'any' });
 
     if (output.type == 'single') {
       return interaction.reply(output.joke);
