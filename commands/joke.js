e3e384dea9c08b7e68bf89f578c2d082d8ccd189
Demian <Devaccdemiann@gmail.com>
2021-10-10 17:10:35 +0200

    Create joke command

---
+++ commands/joke.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const JokeWrapper = require('@qgisk/jokeapi-wrapper');
+const JokeClient = new JokeWrapper();
+
+module.exports = {
+  data: new SlashCommandBuilder().setName('joke').setDescription('Replies with a joke!'),
+  async execute(interaction, settings) {
+    const output = await JokeClient.getJoke({ ...settings });
+
+    if (output.type == 'single') {
+      return interaction.reply(output.joke);
+    }
+
+    await interaction.reply(output.setup);
+    setTimeout(() => {
+      return interaction.followUp(output.delivery);
+    }, 2000);
+  },
+};
